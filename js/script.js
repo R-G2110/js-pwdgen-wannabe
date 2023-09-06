6f672b2236a023387edd8d3c8bd647efef985c21
R-G2110 <argeldelacruz86@gmail.com>
2023-09-06 16:54:37 +0200

    [Edit]: spaces on script.js

--- js/script.js
+++ js/script.js
@@ -8,11 +8,9 @@ Infine scrivi sulla pagina nomecognomecolorepreferito23
 /*Passaggi:
 1. Salvare in 3 variabili il prompt dell'inserimento del nome, cognome e colore preferito.
 
-2. Effettuare un controllo degli spazi vuoti e toglierli.
+2. Creare la stringa di output, trasformare la stringa in minuscolo ed effettuare un controllo degli spazi vuoti e toglierli.
 
-3. Creare la stringa di output.
-
-4. effettuare l'innerHTML dell'output nel tag designato. */
+3. effettuare l'innerHTML dell'output nel tag designato. */
 
 //1.
 const firstName = prompt('Inserici il tuo nome');
@@ -20,10 +18,7 @@ const lastName = prompt('Inserici il tuo cognome');
 const favColor = prompt('Inserici il tuo colore preferito');
 
 //2.
-
-
-//3.
 const generatedPassword = firstName.toLowerCase().trim().replace(" ","")+ lastName.toLowerCase().trim().replace(" ","") + favColor.toLowerCase().trim().replace(" ","") + 23;
 
-//4.
+//3.
 document.getElementById('output').innerHTML = generatedPassword;
