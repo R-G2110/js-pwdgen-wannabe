d433258a868f06e26e96045999a1d806ef4e8557
R-G2110 <argeldelacruz86@gmail.com>
2023-09-07 08:10:28 +0200

    [Edit]: comments on script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ Infine scrivi sulla pagina nomecognomecolorepreferito23
 /*Passaggi:
 1. Salvare in 3 variabili il prompt dell'inserimento del nome, cognome e colore preferito.
 
-2. Effettuare un controllo della stringa se è vuota o non contiene nessun carattere tranne lo spazio
+2. Effettuare un controllo della stringa se è vuota oppure se contiene lo spazio, e dare un messaggio di errore.
 
 3. Creare la stringa di output, trasformare la stringa in minuscolo ed effettuare un controllo degli spazi vuoti e toglierli.
 
@@ -18,18 +18,18 @@ Infine scrivi sulla pagina nomecognomecolorepreferito23
 
 
 const firstName = prompt('Inserici il tuo nome');
-if (firstName.length <= 0 || firstName == null) {
+if (firstName == " " || firstName == "  " || firstName == null) {
 	txt = "Attenzione! inserisci il dato richiesto!";
 } else {
 	const lastName = prompt('Inserici il tuo cognome');
-	if (lastName.length <= 0 || lastName == null){
+	if (lastName == " " || lastName == "  " || lastName == null){
 		txt = "Attenzione! inserisci il dato richiesto!";
 	} else {
 		const favColor = prompt('Inserici il tuo colore preferito');
-		if (favColor.length <= 0 || favColor == null){
+		if (favColor == " " || favColor == "  " || favColor == null){
 			txt = "Attenzione! inserisci il dato richiesto!";
 		} else{
-			const generatedPassword = `${firstName.toLowerCase().trim().replace(" ","")}${lastName.toLowerCase().trim().replace(" ","")}${favColor.toLowerCase().trim().replace(" ","")}23
+			const generatedPassword = ` La password generato per te è: "${firstName.toLowerCase().trim().replace(" ","")}${lastName.toLowerCase().trim().replace(" ","")}${favColor.toLowerCase().trim().replace(" ","")}23"
 			`
 			document.getElementById("output").innerHTML = generatedPassword.replace(" ","");
 		}
